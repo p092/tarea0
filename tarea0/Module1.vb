c7ee840d89a2abcbf3ea81315b75face06c09514
szapeta <2375894860101@ingenieria.usac.edu.gt>
2024-07-26 19:11:22 -0600

    Se completa el proyecto cero

--- tarea0/Module1.vb
+++ tarea0/Module1.vb
@@ -2,6 +2,14 @@
 
     Sub Main()
         Console.WriteLine("Ingresa tu nombre")
+        Dim nombre As String
+        nombre = Console.ReadLine()
+
+        Dim mensaje As String
+
+        mensaje = "Bienvenido " + nombre
+
+        Console.WriteLine(mensaje)
 
         Console.ReadKey()
     End Sub
